3dc01a831d36877d6979fd317a7af0f537a4dc09
vinod kumar <88377243+codervinod123@users.noreply.github.com>
2023-11-22 13:40:25 +0530

    videocontainer fixed

--- src/components/Vediocard.js
+++ src/components/Vediocard.js
@@ -7,11 +7,11 @@ const Vediocard = ({info,theme}) => {
   return (
     <div className=''>
        
-       <div className=' w-56  rounded'>
+       <div className=' w-64  rounded'>
          <img className='rounded' src={info.snippet.thumbnails.medium.url} alt="thumbnail" />
        </div>
 
-       <div className={`flex w-56 py-3 gap-3 ${theme ? 'text-black' : 'text-white' }`}>
+       <div className={`flex w-64 py-3 gap-3 ${theme ? 'text-black' : 'text-white' }`}>
         <div className='h-[70px] w-[70px] '><img className='rounded-full'  src="https://yt3.ggpht.com/5KYJgS9_QFNDW7GJaiGb50tSRV_4I0Ijx5mt5SBsel9HRyy7ppKYJ8bd2TewTXa3bz1jnethU4Q=s68-c-k-c0x00ffffff-no-rj" alt="" /></div>
         
         <div>
